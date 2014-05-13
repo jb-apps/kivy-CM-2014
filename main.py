b5717f7708ee38a133ceae8f6561b220f08ccfda
Jonathan Benavides Vallejo <maxjbv@gmail.com>
2014-05-13 16:26:22 +0200

    mejora de la interfaz de usuario

--- main.py
+++ main.py
@@ -76,12 +76,17 @@ class Utilities():
 	def popupCancelarAceptar(self,txt_title,txt_content, orig, dest):
 		layout = BoxLayout(orientation='vertical')
 		lab = Label(text=txt_content)
-		btnCerrar = Button(text='Cerrar', size_hint=(.5,.2))
-		btnAceptar= Button(text='Aceptar', size_hint=(.5,.2))
+
+		boxlayout = BoxLayout(orientation='horizontal', size_hint= (1, None), height='45sp', spacing='5sp')
+
+		btnCerrar = Button(text='Cerrar')#, size_hint=(.5,.2))
+		btnAceptar= Button(text='Aceptar')#, size_hint=(.5,.2))
 		
 		layout.add_widget(lab)
-		layout.add_widget(btnCerrar)
-		layout.add_widget(btnAceptar)
+		boxlayout.add_widget(btnCerrar)
+		boxlayout.add_widget(btnAceptar)
+
+		layout.add_widget(boxlayout)
 
 		popup = Popup(title=txt_title,content=layout,size_hint=(.8, .5))
 
@@ -142,6 +147,8 @@ class UserListScreen(Screen):
 		list_item_args_converter = \
 			lambda row_index, obj: {'text': '[b]'+obj+'[/b] ---- Ptos: ' + str(js_response['data'][obj]),
                                     'size_hint_y': None,
+                                    'size_hint_x': .8,
+                                    'background_color': [.4, .6, .6, 1],
                                     'selected_color': [.5,.5,.5,1],
                                     'deselected_color': [.3,.3,.3,1],
                                     'markup': True}
